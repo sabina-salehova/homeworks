9b66ee1d2b0bd1a90a7fa3b764f6b28222c71fd7
sabina-salehova <sebine93@gmail.com>
2022-04-13 14:23:18 +0400

    added homeworks 13.04.2022

---
+++ homeworks 13.04.2022/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace homeworks_13._04._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Daxil edilmish ededin tek ve ya cut oldugunun tapilmasi ---");
+
+            Console.Write("Ededi daxil edin: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number + " cut ededdir");
+                }
+                else
+                    Console.WriteLine(number + " tek ededdir");
+            }
+            else
+                Console.WriteLine(number + " Natural eded deyil");
+
+
+            Console.WriteLine("--- Daxil edilmish ededlerden boyuk olanin tapilmasi ---");
+
+            Console.Write("ilk  ededi daxil edin: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ikinci ededi daxil edin: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 == number2)
+            {
+                Console.WriteLine("ededler beraberdir");
+            }
+            else if (number1 > number2)
+            {
+                Console.WriteLine(number1 + " ededi boyukdur");
+            }
+            else
+                Console.WriteLine(number2 + " ededi boyukdur");
+        }
+    }
+}

4fc239fb083668e417cfdb2b219dc4d04a45b37a
[sabina-salehova] <[sabinaas@code.edu.az]>
2022-04-14 14:11:03 +0400

    added homeworks 14.04.2022

---
+++ homeworks 14.04.2022/task_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace rrrtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] arr = createdStringArray();
+
+            Console.Write("\n 0-dan boyuk olmaqla axtarilmasi istenilen element uzunlugu daxil edin: ");
+            int lengthOfElement = FindPositiveNumber();
+
+            Console.WriteLine(" \n--- "+ lengthOfElement+" uzunlugundan boyuk array-in elementleri ---  ");
+            findLengthOfElementInArray(arr, lengthOfElement);
+        }
+
+        public static void findLengthOfElementInArray(string[] arr, int lengthOfElement)
+        {
+            foreach (string item in arr)
+            {
+                if (item.Length > lengthOfElement)
+                    Console.WriteLine(" \n  "+item);
+            }
+        }
+        public static string[] createdStringArray()
+        {
+            Console.Write("\n 0-dan boyuk olmaqla array-in uzunlugunu daxil edin: ");
+            int size = FindPositiveNumber();
+            Console.WriteLine(" \n--- Elementlerin deyerlerini daxil edin ---");
+            string[] arr = new string[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("\n arr[" + i + "]= ");
+                arr[i] = Console.ReadLine();
+            }
+            return arr;
+
+        }
+        public static int FindPositiveNumber()
+        {
+            int number;
+            do
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number <= 0)
+                {
+                    Console.Write(" Duzgun deyil, yeniden deyer daxil edin: ");
+                }
+            } while (number <= 0);
+
+            return number;
+        }
+    }
+}
---
+++ homeworks 14.04.2022/task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace rrtest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n  0-dan kicik olmayan eded daxil edin: ");
+            int number = FindNumber();
+                        
+            Console.WriteLine("\n  Ededin reqemlerinin cemi: " + findTotalElementsOfNumber(number));
+        }
+
+        public static int findTotalElementsOfNumber(int number)
+        {
+            int total = 0, remainder;
+            while (number > 0)
+            {
+                remainder = number % 10;
+                total += remainder;
+                number /= 10;
+            }
+            return total;
+        }
+        public static int FindNumber()
+        {
+            int number;
+            do
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number < 0)
+                {
+                    Console.Write("  Duzgun deyil, yeniden eded daxil edin: ");
+                }
+            } while (number < 0);
+
+            return number;
+        }
+
+
+    }
+}

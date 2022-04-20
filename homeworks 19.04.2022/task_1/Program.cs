4d4164584f0046cd7eab488330c93836bf736d54
[sabina-salehova] <[sabinaas@code.edu.az]>
2022-04-20 16:52:19 +0400

    added homeworks 19.04.2022

---
+++ homeworks 19.04.2022/task_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter modifeded sentences: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Result: "+modifiedString(str));
+        }
+        public static string modifiedString(string str)
+        {          
+            str = str.ToLower();
+            string[] charWordArray = new string[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                charWordArray[i] = str[i].ToString();
+            }
+            
+            string[] charWordArray2 = new string[str.Length];
+            charWordArray2[0] = charWordArray[0].ToUpper();
+            
+            for (int i = 1; i < charWordArray.Length; i++)
+            {
+                
+
+                if (charWordArray[i-1] == " ")
+                {
+                    charWordArray2[i] = charWordArray[i].ToUpper();
+                }
+                else
+                    charWordArray2[i] = charWordArray[i];           
+              }
+            
+            string sentences=null; ;
+            foreach (string item in charWordArray2)
+            {
+                sentences+=item;
+                
+            }           
+           
+            return sentences;
+
+        }
+
+
+
+
+    }
+    }
+
---
+++ homeworks 19.04.2022/task_2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Kamil","Mammadov","P324",79,false);
+            Console.WriteLine(student.sendNameSurname()); ;
+            Console.WriteLine(student.sendStudentFields());
+            student.havingAccessToTheNextExam();
+        }
+    }
+
+       public class Student 
+    {
+        public string name;
+        public string surname;
+        public string group;
+        public int point;
+        public bool isGraudated;
+        public Student(string Name,string Surname,string Group, int Point, bool IsGraudated )
+        {
+            name = Name;
+            surname = Surname;
+            group = Group;
+            point = Point;
+            isGraudated = IsGraudated;
+        }
+
+        public string sendNameSurname()
+        {
+            return string.Concat("Name: ",name," Surname: ",surname);
+        }
+        public string isGraudatedMetod()
+        {
+            if (isGraudated)
+                return "is graudated";
+            else
+                return "is not graudated";
+
+        }
+
+        public string sendStudentFields()
+            
+        {
+            return string.Concat("Name: ", name, " Surname: ", surname," Group: ", group," Point: ", point," IsGraudated: ", isGraudatedMetod());
+        }
+
+        public void havingAccessToTheNextExam()
+        {
+            if (point > 80)
+                Console.WriteLine("Have access to the next exam");
+            else
+                Console.WriteLine("Have not access to the next exam");
+        }
+
+    }
+
+    
+}
